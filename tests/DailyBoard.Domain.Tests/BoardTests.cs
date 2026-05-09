b5a9b1b346bd3d7fd3129c54fec345d0f4244db7
rajiatha <rajithatharaka@gmail.com>
2026-05-10 01:29:55 +0800

    chore: remove tracked build artifacts and clean repository

--- tests/DailyBoard.Domain.Tests/BoardTests.cs
+++ tests/DailyBoard.Domain.Tests/BoardTests.cs
@@ -21,5 +21,57 @@ namespace DailyBoard.Domain.Tests
             Assert.Equal(ownerId, board.OwnerId);
             Assert.NotEqual(Guid.Empty, board.Id);
         }
+
+        [Fact]
+        public void Rename_ShouldUpdateBoardName()
+        {
+            var board = Board.Create("Old Name", Guid.NewGuid());
+            var newName = "New Name";
+            board.Rename(newName);
+            Assert.Equal(newName, board.Name);
+        }
+
+        [Fact]
+        public void AddCard_ShouldAddCardToBoard()
+        {
+            var board = Board.Create("Board", Guid.NewGuid());
+            var card = new Card(
+                Guid.NewGuid(),
+                board.Id,
+                "Card 1",
+                "Description",
+                CardStatus.Todo,
+                DateTime.UtcNow.AddDays(1),
+                1
+            );
+            board.AddCard(card);
+            Assert.Contains(card, board.Cards);
+        }
+
+        [Fact]
+        public void RemoveCard_ShouldRemoveCardFromBoard()
+        {
+            var board = Board.Create("Board", Guid.NewGuid());
+            var card = new Card(
+                Guid.NewGuid(),
+                board.Id,
+                "Card 1",
+                "Description",
+                CardStatus.Todo,
+                DateTime.UtcNow.AddDays(1),
+                1
+            );
+            board.AddCard(card);
+            board.RemoveCard(card.Id);
+            Assert.DoesNotContain(card, board.Cards);
+        }
+
+        [Fact]
+        public void Cards_ShouldBeReadOnly()
+        {
+            var board = Board.Create("Board", Guid.NewGuid());
+            Assert.IsAssignableFrom<IReadOnlyCollection<Card>>(board.Cards);
+        }
+        
     }
 }
